47e811cee3acd997c5bbecbeb0a548b21e366892
1312420 <1312420@student.hcmus.edu.vn>
2018-06-22 10:20:00 +0700

    Add files via upload

--- Database/STORED PROCEDURE/4_DatPhong.sql
+++ Database/STORED PROCEDURE/4_DatPhong.sql
@@ -1,51 +1,76 @@
 ﻿USE [QuanLyKhachSan]
 GO
-
-CREATE PROCEDURE SP_UpdateStatus
-	@NUM_ROOM int, @DATE datetime, @STATUS int
-AS
-BEGIN
-	UPDATE TrangThaiPhong
-	SET tinhTrang = @STATUS
-	WHERE maPhong = @NUM_ROOM and ngay = @DATE
-END
+/****** Object:  StoredProcedure [dbo].[SP_Booking]    Script Date: 6/22/18 9:10:52 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
 GO
 
--- RESULT = 0 // FAIL
--- RESULT = 1 // SUCCESS
-CREATE PROCEDURE SP_Booking 
+
+
+-- RESULT = 0 // SUCCESS
+-- RESULT = 1 // FAIL
+ALTER PROCEDURE [dbo].[SP_Booking] 
 (  
-	@USER nvarchar(20), @NUM_ROOM int, @CHECK_IN datetime, @CHECK_OUT datetime,
-	@RESULT int OUTPUT
+	@USER nvarchar(20), @NUM_ROOM int, @CHECK_IN datetime, @CHECK_OUT datetime
 )
 AS
 BEGIN  
+	--CHECK VALUES OF CHECK_IN
+	IF (@CHECK_IN < GETDATE() )
+	BEGIN
+		PRINT N'YÊU CẦU KHÔNG ĐƯỢC THỰC HIỆN ! NGÀY NHẬN PHÒNG NHỎ HƠN NGÀY ĐẶT PHÒNG'
+		RETURN 1
+	END
+
+	--CHECK VALUES OF CHECK_OUT
+	IF (@CHECK_OUT < @CHECK_IN )
+	BEGIN
+		PRINT N'YÊU CẦU KHÔNG ĐƯỢC THỰC HIỆN ! NGÀY TRẢ PHÒNG NHỎ HƠN NGÀY NHẬN PHÒNG'
+		RETURN 1
+	END
+
+	--CHECK EXISTENCE OF USER
+	IF (NOT(EXISTS(SELECT * FROM KhachHang kh WHERE @USER = kh.tenDangNhap)))
+	BEGIN
+		PRINT N'YÊU CẦU KHÔNG ĐƯỢC THỰC HIỆN ! TÀI KHOẢN KHÔNG TỒN TẠI !'
+		RETURN 1 
+	END
+
+	--GET TYPE OF ROOM
+	DECLARE @TYPE int
+	SELECT @TYPE = P.loaiPhong
+	FROM Phong P
+	WHERE @NUM_ROOM = P.maPhong
+
 	--GET NUM OF GUEST
 	DECLARE @NUM_GUEST int
 	SELECT @NUM_GUEST = KH.maKH
 	FROM KhachHang KH
 	WHERE @USER = KH.tenDangNhap
 
+	DECLARE @RESULT int
+	SET @RESULT = 0;
 	--GET STATUS OF THIS ROOM
 	DECLARE @STATUS int, @DATE datetime;
-	SELECT @STATUS = tt.tinhTrang, @DATE = MAX(tt.ngay) FROM Phong p join TrangThaiPhong tt ON p.maPhong = tt.maPhong
-														WHERE p.maPhong = @NUM_ROOM
-														GROUP BY tt.ngay, tt.tinhTrang
-
-	SET @RESULT = 0;
-
+	SELECT @STATUS = tt.tinhTrang, @DATE = MAX(tt.ngay) 
+	FROM Phong p,TrangThaiPhong tt  
+	WHERE p.maPhong = @NUM_ROOM AND p.maPhong = tt.maPhong
+	GROUP BY tt.ngay, tt.tinhTrang
+	
 	--CHECK STATUS OF THIS ROOM
 	IF (@STATUS = 0 ) 
 		BEGIN
 
 		--GET DATA
 		DECLARE @TARIFF money, @DETAIL nvarchar(200);
-		SELECT @TARIFF = loai.donGia, @DETAIL = loai.moTa FROM Phong p JOIN LoaiPhong loai ON p.loaiPhong = loai.maLoaiPhong
-														  WHERE p.maPhong = @NUM_ROOM
+		SELECT @TARIFF = loai.donGia, @DETAIL = loai.moTa 
+		FROM Phong p ,LoaiPhong loai 
+		WHERE p.maPhong = @NUM_ROOM AND p.loaiPhong = loai.maLoaiPhong
 
 		--SAVE INFORMATION OF THIS ROOM
 		INSERT INTO DatPhong (maLoaiPhong, maKH, ngayBatDau, ngayTraPhong, ngayDat, donGia, moTa, tinhTrang)
-		VALUES (@NUM_ROOM, @NUM_GUEST, @CHECK_IN, @CHECK_OUT, GETDATE(), @TARIFF, @DETAIL, 1)
+		VALUES (@TYPE, @NUM_GUEST, @CHECK_IN, @CHECK_OUT, GETDATE(), @TARIFF, @DETAIL, 1)
 
 		--UPDATE STATUS
 		EXEC sp_UpdateStatus @NUM_ROOM, @DATE, 1
@@ -53,8 +78,16 @@ BEGIN
 		--BOOKING IS SUCCESSFUL
 		SET @RESULT = 1;  
 		PRINT N'Đã đặt phòng thành công.'
+		RETURN 0
 		END
 
-	ELSE PRINT N'Xin lỗi, phòng bạn chọn đã có người đặt. Mời bạn chọn phòng khác.';
-		
+	ELSE 
+		BEGIN
+		PRINT N'Xin lỗi, phòng bạn chọn đã có người đặt. Mời bạn chọn phòng khác.';
+		RETURN 1
+		END
 END
+
+
+
+
